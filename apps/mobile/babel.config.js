bff011ca8c622a558f48183d06ff9ba9c1c319cf
alexmps2003 <126064822+alexmps2003@users.noreply.github.com>
2026-07-02 22:01:09 +0530

    mobile: configure Expo Router and NativeWind

---
+++ apps/mobile/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        require.resolve('expo/node_modules/babel-preset-expo'),
+        { jsxImportSource: 'nativewind' },
+      ],
+      'nativewind/babel',
+    ],
+  };
+};
